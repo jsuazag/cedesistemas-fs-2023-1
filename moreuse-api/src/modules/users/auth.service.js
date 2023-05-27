b58487076df827025a7cda4a8365ff4e5513d53f
Jerson Suaza <jerson@netuxtecnologia.com>
2023-05-27 10:07:31 -0500

    login and signup services

--- moreuse-api/src/modules/users/auth.service.js
+++ moreuse-api/src/modules/users/auth.service.js
@@ -1,12 +1,21 @@
 const errorHandler = require('../../utils/errorHandler');
 const User = require('./models/user.model');
-const { USER_PASS_WRONG, SERVER_ERROR } = require('./utils/dict.errors');
+const bcrypt = require('bcrypt');
 
-const login = (email, password) => {
+const { USER_PASS_WRONG, SERVER_ERROR,
+        USER_ALREDY_EXISTS, USER_NOT_FOUND
+      } = require('./utils/dict.errors');
+
+const login = async (email, password) => {
   try {
-    if (email === "juanito@gmail.com" && password === "123") {
-      return {
-        token: 'xxxxyyyyzzzz'
+    const user = await User.findOne({ email });
+    if (user) {
+      const match = await bcrypt.compare(password, user.password);
+      if (match) {
+        // TODO: generate token
+        return {
+          token: 'xxxxxxxxxyyyyyyzzzzz'
+        }
       }
     }
     throw errorHandler(USER_PASS_WRONG);
@@ -23,6 +32,14 @@ const logout = (idUser) => {
 
 const signup = async (userData) => {
   try {
+    const validateUser = await User.findOne({ email: userData.email });
+    if (validateUser) {
+      throw errorHandler(USER_ALREDY_EXISTS)
+    }
+
+    const passHashed = await bcrypt.hash(userData.password, 10);
+    userData.password = passHashed;
+
     const user = User(userData);
     await user.save(); // -> insert_one({ ... })
     return {
@@ -34,12 +51,13 @@ const signup = async (userData) => {
   }
 }
 
-const info = (idUser) => {
-  return {
-    name: 'Juanito Perez',
-    email: "juanito@gmail.com",
-    address: "Calle siempre viva",
-    phone: "11111"
+const info = async (idUser) => {
+  try {
+    const user = await User.findById (idUser);
+    //return user ? user : errorHandler(USER_NOT_FOUND);
+    return user || errorHandler(USER_NOT_FOUND);
+  } catch (error) {
+    throw error.handled ? error : errorHandler(SERVER_ERROR, error);
   }
 }
 
--- moreuse-api/src/modules/users/utils/dict.errors.js
+++ moreuse-api/src/modules/users/utils/dict.errors.js
@@ -3,6 +3,14 @@ const dictErrors = {
     status: 404,
     message: "user or password wrong"
   },
+  USER_ALREDY_EXISTS: {
+    status: 409,
+    message: "user already exists"
+  },
+  USER_NOT_FOUND: {
+    status: 404,
+    message: 'user not found'
+  },
   SERVER_ERROR: {
     status: 500,
     message: "server internal error"
